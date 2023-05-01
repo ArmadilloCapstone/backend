c07c0f379bdf6da885e1da5efa7647f649c52408
jaeyoon Cheong <jyjung9910@gmail.com>
2023-05-01 20:31:56 +0900

    student_time domain 추가

---
+++ dolbomi/src/main/java/com/example/dolbomi/domain/Student_time.java
@@ -0,0 +1,114 @@
+package com.example.dolbomi.domain;
+
+import java.util.Date;
+
+public class Student_time {
+    private Long id;
+    private Long student_id;
+    private Date entry_1;
+    private Date entry_2;
+    private Date entry_3;
+    private Date entry_4;
+    private Date entry_5;
+    private Date off_1;
+    private Date off_2;
+    private Date off_3;
+    private Date off_4;
+    private Date off_5;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getStudent_id() {
+        return student_id;
+    }
+
+    public void setStudent_id(Long student_id) {
+        this.student_id = student_id;
+    }
+
+    public Date getEntry_1() {
+        return entry_1;
+    }
+
+    public void setEntry_1(Date entry_1) {
+        this.entry_1 = entry_1;
+    }
+
+    public Date getEntry_2() {
+        return entry_2;
+    }
+
+    public void setEntry_2(Date entry_2) {
+        this.entry_2 = entry_2;
+    }
+
+    public Date getEntry_3() {
+        return entry_3;
+    }
+
+    public void setEntry_3(Date entry_3) {
+        this.entry_3 = entry_3;
+    }
+
+    public Date getEntry_4() {
+        return entry_4;
+    }
+
+    public void setEntry_4(Date entry_4) {
+        this.entry_4 = entry_4;
+    }
+
+    public Date getEntry_5() {
+        return entry_5;
+    }
+
+    public void setEntry_5(Date entry_5) {
+        this.entry_5 = entry_5;
+    }
+
+    public Date getOff_1() {
+        return off_1;
+    }
+
+    public void setOff_1(Date off_1) {
+        this.off_1 = off_1;
+    }
+
+    public Date getOff_2() {
+        return off_2;
+    }
+
+    public void setOff_2(Date off_2) {
+        this.off_2 = off_2;
+    }
+
+    public Date getOff_3() {
+        return off_3;
+    }
+
+    public void setOff_3(Date off_3) {
+        this.off_3 = off_3;
+    }
+
+    public Date getOff_4() {
+        return off_4;
+    }
+
+    public void setOff_4(Date off_4) {
+        this.off_4 = off_4;
+    }
+
+    public Date getOff_5() {
+        return off_5;
+    }
+
+    public void setOff_5(Date off_5) {
+        this.off_5 = off_5;
+    }
+}
