1e3c73985dad40705bfb67258a1633e30a70a30e
jaeyoon Cheong <jyjung9910@gmail.com>
2023-05-01 20:28:39 +0900

    after_school_class domain 추가

--- dolbomi/src/main/java/com/example/dolbomi/domain/After_school_class.java
+++ dolbomi/src/main/java/com/example/dolbomi/domain/After_school_class.java
@@ -1,2 +1,51 @@
-package com.example.dolbomi.domain;public class After_school_class {
+package com.example.dolbomi.domain;
+
+import java.util.Date;
+
+public class After_school_class {
+    private Long id;
+    private String class_name;
+    private Date start_time;
+    private Date end_time;
+    private Long day;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getClass_name() {
+        return class_name;
+    }
+
+    public void setClass_name(String class_name) {
+        this.class_name = class_name;
+    }
+
+    public Date getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(Date start_time) {
+        this.start_time = start_time;
+    }
+
+    public Date getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(Date end_time) {
+        this.end_time = end_time;
+    }
+
+    public Long getDay() {
+        return day;
+    }
+
+    public void setDay(Long day) {
+        this.day = day;
+    }
 }
