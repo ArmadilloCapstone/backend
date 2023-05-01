7291f0fab1eac89dd25c4e5abe76c0e31194a15a
jaeyoon Cheong <jyjung9910@gmail.com>
2023-05-01 19:58:54 +0900

    Controller -> controller

--- dolbomi/src/main/java/com/example/dolbomi/controller/HelloWorldController.java
+++ dolbomi/src/main/java/com/example/dolbomi/controller/HelloWorldController.java
@@ -1,15 +1,13 @@
-package com.example.dolbomi.controller;
+package com.example.dolbomi.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
-
-@RestController
+@Controller
 public class HelloWorldController {
-    @GetMapping("/hello")
-    public List<String> hello(){
-        return Arrays.asList("안녕하세요","hello");
+    @GetMapping("/")
+    public String test(){
+        return "index.html";
     }
 }
