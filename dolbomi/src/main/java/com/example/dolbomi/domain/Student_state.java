c0a0737b988f3e8fb54dcd44f38efc652b6c80e1
jaeyoon Cheong <jyjung9910@gmail.com>
2023-05-01 20:30:18 +0900

    student_state domain 추가

---
+++ dolbomi/src/main/java/com/example/dolbomi/domain/Student_state.java
@@ -0,0 +1,31 @@
+package com.example.dolbomi.domain;
+
+public class Student_state {
+    private Long id;
+    private Long student_id;
+    private Long state;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getStudent_id() {
+        return student_id;
+    }
+
+    public void setStudent_id(Long student_id) {
+        this.student_id = student_id;
+    }
+
+    public Long getState() {
+        return state;
+    }
+
+    public void setState(Long state) {
+        this.state = state;
+    }
+}
