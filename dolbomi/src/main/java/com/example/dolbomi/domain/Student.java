47514f5f326f6c191586a63043d003ddcf6031f5
jaeyoon Cheong <jyjung9910@gmail.com>
2023-05-01 20:21:58 +0900

    student domain 추가

--- dolbomi/src/main/java/com/example/dolbomi/domain/Student.java
+++ dolbomi/src/main/java/com/example/dolbomi/domain/Student.java
@@ -1,8 +1,17 @@
 package com.example.dolbomi.domain;
 
+import java.util.Date;
+
 public class Student {
+
     private Long id;
     private String name;
+    private Long grade;
+    private String phone_num;
+    private Long gender;
+    private Long class_id;
+    private Date birth_date;
+    private Long disable;
 
     public Long getId() {
         return id;
@@ -18,5 +27,53 @@ public class Student {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Long getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Long grade) {
+        this.grade = grade;
+    }
+
+    public String getPhone_num() {
+        return phone_num;
+    }
+
+    public void setPhone_num(String phone_num) {
+        this.phone_num = phone_num;
+    }
+
+    public Long getGender() {
+        return gender;
+    }
+
+    public void setGender(Long gender) {
+        this.gender = gender;
+    }
+
+    public Long getClass_id() {
+        return class_id;
+    }
+
+    public void setClass_id(Long class_id) {
+        this.class_id = class_id;
+    }
+
+    public Date getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(Date birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public Long getDisable() {
+        return disable;
+    }
+
+    public void setDisable(Long disable) {
+        this.disable = disable;
     }
 }
