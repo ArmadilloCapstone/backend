f735bc71d42e5c08dbba115d9a32adbdb41eb71b
cocosome <kcsgaram123@gmail.com>
2023-05-06 01:20:05 +0900

    fetch에서 axios형태로 data전달 방식 변경,  get에서 post방식으로 data전달 방ìì식 변êê경

--- dolbomi/frontend/src/App.js
+++ dolbomi/frontend/src/App.js
@@ -1,18 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from "react";
+import axios from 'axios';
 
 function App() {
-  const [message, setMessage] = useState([]);
+  const [guardianMessage, setGuardianMessage] = useState([]);
+  const [parentMessage, setParentMessage] = useState([]);
+  const [parentRequest, setParentRequest] = useState([]);
   useEffect(() => {
-          fetch("/hello")
-              .then((response) => {
-                  return response.json();
-              })
-              .then(function (data) {
-                  setMessage(data);
-              });
-      }, []);
+          //getUser();
+          getParent();
+          getGuardian();
+          getParentRequest();
+  },[]);
+
+//    async function getUser(){
+//        await axios
+//            .get("/hello")
+//            .then((response) => {
+//                console.log(response.data);
+//                setMessage(response.data);
+//            })
+//            .catch((error)=>{
+//                console.log(error);
+//            })
+//    }
+
+    async function getParent(){
+        await axios
+            .post("/parent")
+            .then((response) => {
+               console.log(response.data);
+               setParentMessage(response.data);
+            })
+            .catch((error)=>{
+                console.log(error);
+            })
+    }
+
+    async function getGuardian(){
+            await axios
+                .post("/guardian")
+                .then((response) => {
+                    console.log(response.data);
+                    setGuardianMessage(response.data);
+                })
+                .catch((error)=>{
+                    console.log(error);
+                })
+    }
+    async function getParentRequest(){
+            await axios
+                .post("/requestParent")
+                .then((response) => {
+                    console.log(response.data);
+                    setParentRequest(response.data);
+                })
+                .catch((error)=>{
+                    console.log(error);
+                })
+   }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -28,9 +78,30 @@ function App() {
         >
           Learn React
         </a>
+        <h1> Parent Data </h1>
         <ul>
-            {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
+            {
+                Object.entries(parentMessage).map((text,index) =>
+                <li key={`${index}`}> {text[0]} : {text[1]} </li>)
+            }
         </ul>
+        <br/>
+        <h1> Guardian Data </h1>
+        <ul>
+            {
+                guardianMessage.map((obj, index) =>
+                Object.entries(obj).map((text,idx) =>
+                <li key={`${idx}`}> {text[0]} : {text[1]} </li>))
+            }
+        </ul>
+        <br/>
+        <h1> Parent Request Data </h1>
+                <ul>
+                    {
+                        Object.entries(parentRequest).map((text,index) =>
+                        <li key={`${index}`}> {text[0]} : {text[1]} </li>)
+                    }
+                </ul>
       </header>
     </div>
   );
---
+++ dolbomi/src/main/java/com/example/dolbomi/controller/ParentPickupRequestForm.java
@@ -0,0 +1,2 @@
+package com.example.dolbomi.controller;public class ParentPickupRequestForm {
+}
