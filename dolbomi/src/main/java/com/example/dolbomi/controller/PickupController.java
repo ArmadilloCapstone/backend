8e510ea0230dd13746809af7138b7d6e194adaae
cocosome <kcsgaram123@gmail.com>
2023-05-21 21:23:17 +0900

    pickup controller 더미데이터부분 수정

--- dolbomi/src/main/java/com/example/dolbomi/controller/PickupController.java
+++ dolbomi/src/main/java/com/example/dolbomi/controller/PickupController.java
@@ -31,52 +31,21 @@ public class PickupController {
     */
 
     @PostMapping("/guardian")
-    public List<StudentPickupForm> guardianPickup(){
-        Guardian guardian = new Guardian();
-        guardian.setId(0001L);
-        return pickupService.selectStudentForGuardian(guardian);
-    }
-    /*
-    @PostMapping("/guardian")
     public List<StudentPickupForm> guardianPickup(@RequestBody Guardian guardian){
         return pickupService.selectStudentForGuardian(guardian);
     }
-    */
+
     @PostMapping("/requestParent")
     public String parentRequest(@RequestBody PickupRequestForm pickupRequestForm){
         return pickupService.requestPickupByParent(pickupRequestForm.getPickupManId());
     }
 
     @PostMapping("/requestGuardian")
-    public List<PickupRequestForm> guardianRequest(){
-        Guardian guardian = new Guardian();
-        guardian.setId(100L);
-        guardian.setName("이수성");
-
-        List<StudentPickupForm> studentPickupFormList = new ArrayList<>();
-        StudentPickupForm studentPickupForm1 = new StudentPickupForm();
-        studentPickupForm1.setId(20L);
-        studentPickupForm1.setName("강하늘");
-        studentPickupForm1.setGrade(1L);
-        studentPickupForm1.setGender(1L);
-
-        StudentPickupForm studentPickupForm2 = new StudentPickupForm();
-        studentPickupForm2.setId(30L);
-        studentPickupForm2.setName("최익현");
-        studentPickupForm2.setGrade(2L);
-        studentPickupForm2.setGender(2L);
-
-        studentPickupFormList.add(studentPickupForm1);
-        studentPickupFormList.add(studentPickupForm2);
-        return pickupService.requestPickupByGuardian(guardian, studentPickupFormList);
-    }
-    /*
-    @PostMapping("/requestGuardian")
     public List<PickupRequestForm> guardianRequest(@RequestBody Guardian guardian,
                                                    @RequestBody List<StudentPickupForm> studentPickupFormList){
         return pickupService.requestPickupByGuardian(guardian, studentPickupFormList);
     }
-    */
+
     @PostMapping("/sendPickupFormToTeacher")
     public List<PickupRequestForm> teacherSend(){
         return pickupService.sendPickupForTeacher();
