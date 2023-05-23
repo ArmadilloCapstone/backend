685bcb25db58155116dcdf3b7dcd1323352bab9d
cocosome <kcsgaram123@gmail.com>
2023-05-23 13:15:34 +0900

    commit

--- dolbomi/src/main/java/com/example/dolbomi/controller/AdminController.java
+++ dolbomi/src/main/java/com/example/dolbomi/controller/AdminController.java
@@ -15,88 +15,142 @@ public class AdminController {
     private AdminService adminService;
 
     @Autowired
-    public AdminController(AdminService adminService){
+    public AdminController(AdminService adminService) {
         this.adminService = adminService;
     }
 
     @PostMapping("/dolbom_class_submit")
-    public void addNewDolbomClass(@RequestBody DolbomClass dolbomClass){
+    public void addNewDolbomClass(@RequestBody DolbomClass dolbomClass) {
         adminService.addNewDolbomClass(dolbomClass);
     }
+
     @DeleteMapping("/dolbom_class/{productId}")
-    public void deleteDolbomClass(@PathVariable("productId") Long productId){ adminService.deleteDolbomClass(productId); }
+    public void deleteDolbomClass(@PathVariable("productId") Long productId) {
+        adminService.deleteDolbomClass(productId);
+    }
+
     @PostMapping("/dolbom_class")
-    public List<DolbomClass> sendDolbomClass(){
+    public List<DolbomClass> sendDolbomClass() {
         return adminService.sendDolbomClassList();
     }
 
     @PostMapping("/student_submit")
-    public void addNewStudentManageForm(@RequestBody StudentManageForm studentManageForm){ adminService.addNewStudentManageForm(studentManageForm);}
+    public void addNewStudentManageForm(@RequestBody StudentManageForm studentManageForm) {
+        adminService.addNewStudentManageForm(studentManageForm);
+    }
+
     @PostMapping("/student_submit_csv")
     public void addNewStudentByCsv(@RequestParam("file") List<MultipartFile> files) {
-        for(int i = 0; i < files.size(); i++){
+        for (int i = 0; i < files.size(); i++) {
             adminService.addNewStudentByCsv(files.get(i));
         }
     }
+
     @DeleteMapping("/student/{productId}")
-    public void deleteStudent(@PathVariable("productId") Long productId){
+    public void deleteStudent(@PathVariable("productId") Long productId) {
         adminService.deleteStudent(productId);
     }
+
     @PostMapping("/student")
-    public List<Student> sendStudent(){
+    public List<Student> sendStudent() {
         return adminService.sendStudentList();
     }
+
     @PostMapping("/student/dolbom_classList")
-    public List<DolbomClass> sendDolbomClassForStudent() { return adminService.sendDolbomClassList(); }
+    public List<DolbomClass> sendDolbomClassForStudent() {
+        return adminService.sendDolbomClassList();
+    }
 
     @PostMapping("/teacher_submit")
-    public void addNewTeacherManageForm(@RequestBody TeacherManageForm teacherManageForm) { adminService.addNewTeacherManageForm(teacherManageForm); }
+    public void addNewTeacherManageForm(@RequestBody TeacherManageForm teacherManageForm) {
+        adminService.addNewTeacherManageForm(teacherManageForm);
+    }
+
     @DeleteMapping("/teacher/{productId}")
-    public void deleteTeacher(@PathVariable("productId") Long productId) { adminService.deleteTeacher(productId); }
+    public void deleteTeacher(@PathVariable("productId") Long productId) {
+        adminService.deleteTeacher(productId);
+    }
+
     @PostMapping("/teacher")
-    public List<TeacherManageForm> sendTeacher() { return adminService.sendTeacherList(); }
+    public List<TeacherManageForm> sendTeacher() {
+        return adminService.sendTeacherList();
+    }
+
     @PostMapping("/teacher/dolbom_classList")
-    public List<DolbomClass> sendDolbomClassForTeacher() { return adminService.sendDolbomClassList(); }
+    public List<DolbomClass> sendDolbomClassForTeacher() {
+        return adminService.sendDolbomClassList();
+    }
 
     @PostMapping("/parent_submit")
-    public void addNewParentManageForm(@RequestBody ParentManageForm parentManageForm) { adminService.addNewParentManageForm(parentManageForm); }
+    public void addNewParentManageForm(@RequestBody ParentManageForm parentManageForm) {
+        adminService.addNewParentManageForm(parentManageForm);
+    }
+
     @DeleteMapping("/parent/{productId}")
-    public void deleteParent(@PathVariable("productId") Long productId) { adminService.deleteParent(productId); }
+    public void deleteParent(@PathVariable("productId") Long productId) {
+        adminService.deleteParent(productId);
+    }
+
     @PostMapping("/parent")
-    public List<ParentManageForm> sendParent() { return adminService.sendParentList(); }
+    public List<ParentManageForm> sendParent() {
+        return adminService.sendParentList();
+    }
+
     @PostMapping("/parent/studentList")
-    public List<Student> sendStudentForParent() { return adminService.sendStudentList(); }
+    public List<Student> sendStudentForParent() {
+        return adminService.sendStudentList();
+    }
 
     @PostMapping("/after_school_class_submit")
-    public void addNewAfterSchoolClass(@RequestBody AfterSchoolClass afterSchoolClass) { adminService.addNewAfterSchoolClass(afterSchoolClass); }
+    public void addNewAfterSchoolClass(@RequestBody AfterSchoolClass afterSchoolClass) {
+        adminService.addNewAfterSchoolClass(afterSchoolClass);
+    }
+
     @DeleteMapping("/after_school_class/{productId}")
     public String deleteAfterSchoolClass(@PathVariable("productId") Long productId) {
         int result = adminService.deleteAfterSchoolClass(productId);
-        if(result == 0){
+        if (result == 0) {
             return "해당 방과후활동을 듣고있는 학생이 있으므로 삭제할 수 없습니다";
-        }
-        else if(result == 1){
+        } else if (result == 1) {
             return "삭제되었습니다";
-        }
-        else{
+        } else {
             return "존재하지 않는 방과후활동";
         }
     }
+
     @PostMapping("/after_school_class")
-    public List<AfterSchoolClass> sendAfterSchoolClass() { return adminService.sendAfterSchoolClassList(); }
+    public List<AfterSchoolClass> sendAfterSchoolClass() {
+        return adminService.sendAfterSchoolClassList();
+    }
 
     @PostMapping("/student_schedule_submit")
-    public void addNewStudentSchedule(@RequestBody StudentScheduleForm studentScheduleForm) { adminService.addNewStudentSchedule(studentScheduleForm);}
+    public void addNewStudentSchedule(@RequestBody StudentScheduleForm studentScheduleForm) {
+        adminService.addNewStudentSchedule(studentScheduleForm);
+    }
+
     @DeleteMapping("/student_schedule/{productId}")
-    public void deleteStudentSchedule(@PathVariable("productId") Long productId) { adminService.deleteStudentSchedule(productId); }
+    public void deleteStudentSchedule(@PathVariable("productId") Long productId) {
+        adminService.deleteStudentSchedule(productId);
+    }
+
     @PostMapping("/student_schedule")
-    public List<StudentScheduleForm> sendStudentSchedule() { return adminService.sendStudentSchedule(); }
+    public List<StudentScheduleForm> sendStudentSchedule() {
+        return adminService.sendStudentSchedule();
+    }
 
     @PostMapping("/student_time_submit")
-    public void addNewStudentTime(@RequestBody StudentTime studentTime) { adminService.addNewStudentTime(studentTime); }
+    public void addNewStudentTime(@RequestBody StudentTime studentTime) {
+        adminService.addNewStudentTime(studentTime);
+    }
+
     @DeleteMapping("/student_time/{productId}")
-    public void deleteStudentTime(@PathVariable("productId") Long productId) { adminService.deleteStudentTime(productId); }
+    public void deleteStudentTime(@PathVariable("productId") Long productId) {
+        adminService.deleteStudentTime(productId);
+    }
+
     @PostMapping("/student_time")
-    public List<StudentTime> sendStudentTime() { return adminService.sendStudentTimeList(); }
+    public List<StudentTime> sendStudentTime() {
+        return adminService.sendStudentTimeList();
+    }
 
 }
