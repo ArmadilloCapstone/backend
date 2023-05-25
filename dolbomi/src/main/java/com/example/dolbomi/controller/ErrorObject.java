333ea89ce4648ecf82b0beb8ecb06b7ad6b0d5ed
jaeyoon Cheong <jyjung9910@gmail.com>
2023-05-25 20:55:14 +0900

    회원가입 validation 로직 추가

---
+++ dolbomi/src/main/java/com/example/dolbomi/controller/ErrorObject.java
@@ -0,0 +1,14 @@
+package com.example.dolbomi.controller;
+
+public class ErrorObject {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
