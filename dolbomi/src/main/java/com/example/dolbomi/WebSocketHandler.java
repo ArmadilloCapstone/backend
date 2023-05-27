6e108fcf779d922af9e04c1d92c67f00bc314a8f
Cheong JaeYoon <jyjung9910@gmail.com>
2023-05-27 22:46:44 +0900

    소켓 통신

--- dolbomi/src/main/java/com/example/dolbomi/WebSocketHandler.java
+++ dolbomi/src/main/java/com/example/dolbomi/WebSocketHandler.java
@@ -1,29 +1,73 @@
 package com.example.dolbomi;
 
+import org.apache.tomcat.util.json.JSONParser;
+import org.json.JSONObject;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class WebSocketHandler extends TextWebSocketHandler {
 
-    private final ConcurrentHashMap<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, WebSocketSession> sessionMap = new ConcurrentHashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 
-        String session_id = session.getId();
-        sessions.put(session_id, session);
+        if (session == null) {
+            session.close(CloseStatus.BAD_DATA);
+            return;
+        }
+    }
 
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        String userId = "";
+        for (String key : sessionMap.keySet()) {
+            if(session.equals(sessionMap.get(key))){
+                userId = key;
+            }
+        }
+        if (userId != null) {
+            sessionMap.remove(userId);
+        }
 
-        super.afterConnectionEstablished(session);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        super.handleTextMessage(session, message);
+        //setting_user_id
+        String text = message.getPayload();
+        if(text.length() > 16 && text.substring(0, 15).equals("setting_user_id")){
+            String userId = text.substring(16, text.length());
+            sessionMap.put(userId, session);
+            session.sendMessage(new TextMessage(userId));
+        }
+        else{
+            System.out.println("HI");
+            String userId = "";
+            for (String key : sessionMap.keySet()) {
+                if(session.equals(sessionMap.get(key))){
+                    userId = key;
+                }
+            }
+            System.out.println(message.getPayload());
+            JSONObject chat = new JSONObject(message.getPayload());
+
+            var targetSession = sessionMap.get(chat.getString("id"));
+            System.out.println(chat.getString("id"));;
+            System.out.println(chat.getString("text"));
+            System.out.println(targetSession);
+            if (targetSession == null || userId == null) {
+                return;
+            }
+
+            targetSession.sendMessage(new TextMessage("{ 'id':" + userId+ ",'text':'" + chat.getString("text") + "'}"));
+        }
+
     }
 
     @Override
@@ -31,8 +75,4 @@ public class WebSocketHandler extends TextWebSocketHandler {
         super.handleTransportError(session, exception);
     }
 
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        super.afterConnectionClosed(session, status);
-    }
 }
