40dbf5e044e95f1ddd696a5a1144f67ea8636644
Cheong JaeYoon <jyjung9910@gmail.com>
2023-06-04 01:24:54 +0900

    message DB 구현

--- dolbomi/src/main/java/com/example/dolbomi/repository/MessageRepository.java
+++ dolbomi/src/main/java/com/example/dolbomi/repository/MessageRepository.java
@@ -2,6 +2,8 @@ package com.example.dolbomi.repository;
 
 import com.example.dolbomi.domain.Message;
 import com.example.dolbomi.domain.News;
+import com.example.dolbomi.domain.Parent;
+import com.example.dolbomi.domain.Teacher;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -11,4 +13,12 @@ public interface MessageRepository {
     Optional<Message> findById(Long id);
     Message save(Message message);
     void delete(Long no);
+
+    List<Parent> getAllParentByTid(Long id);
+
+    List<Teacher> getAllTeacherByPid(Long id);
+
+    List<Message> getAllMessageByTid(String s);
+
+    List<Message> getAllMessageByPid(String s);
 }
